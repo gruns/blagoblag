033fc0d65530a2ea167ff6f5a95f2d6631bd31e8
Alex Gaynor <alex.gaynor@gmail.com>
2013-10-20 16:14:09 -0700

    Make it easier to deploy

--- blago.py
+++ blago.py
@@ -1,7 +1,11 @@
 import datetime
+import os
 
+from liquidluck.options import settings, g
 from liquidluck.readers.base import Post
 from liquidluck.readers.restructuredtext import RestructuredTextReader
+from liquidluck.writers import core
+from liquidluck.writers.base import get_post_slug
 
 
 class DateFromPathPost(Post):
@@ -36,3 +40,30 @@ class DateFromPathPost(Post):
 
 class DateFromPathRestructuredTextReader(RestructuredTextReader):
     post_class = DateFromPathPost
+
+
+def get_post_destination(post, slug_format):
+    slug = get_post_slug(post, slug_format)
+
+    if slug.endswith('.html'):
+        return slug
+    elif slug.endswith("/"):
+        return slug + 'index.html'
+    else:
+        return slug + '.html'
+
+
+class DirectoryPostWriter(core.PostWriter):
+    def _dest_of(self, post):
+        dest = get_post_destination(post, settings.config['permalink'])
+        return os.path.join(g.output_directory, dest)
+
+
+class DirectoryPageWriter(core.PageWriter):
+    def start(self):
+        l = len(g.source_directory) + 1
+        for post in g.pure_pages:
+            template = post.template or self._template
+            filename = os.path.splitext(post.filepath[l:])[0] + '/index.html'
+            dest = os.path.join(g.output_directory, filename)
+            self.render({'post': post}, template, dest)
